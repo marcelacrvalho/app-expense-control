35f66249eda206712268d27f503d7564c7b3c013
marcelacrvalho <marcelacrvalho@icloud.com>
2022-11-01 14:13:40 -0300

    Exibe modal ao clicar na action da AppBar

--- lib/app/modules/components/home_view/action_icon_component.dart
+++ lib/app/modules/components/home_view/action_icon_component.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 
 import '../../../themes/themes.dart';
+import '../../controllers/controllers.dart';
+import '../../widgets/widgets.dart';
 
-class CustomActionIcon extends StatelessWidget {
-  const CustomActionIcon({Key? key}) : super(key: key);
+class CustomActionIconWidget extends StatelessWidget {
+  const CustomActionIconWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: const Icon(Icons.add),
-      color: appThemeData.scaffoldBackgroundColor,
-      iconSize: 30.0, 
-      onPressed: () {  },
-    );
+        icon: const Icon(Icons.add),
+        color: appThemeData.scaffoldBackgroundColor,
+        iconSize: 30.0,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                color: appThemeData.appBarTheme.backgroundColor,
+                height: 400.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      CustomTextFieldWidget(
+                        controller: TransactionController.to.titleController,
+                        hintText: 'Nome',
+                      ),
+                      const Divider(),
+                      CustomTextFieldWidget(
+                        controller: TransactionController.to.valueController,
+                        hintText: 'Valor',
+                        onDone: TransactionController.to.newTransaction,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        });
   }
 }
