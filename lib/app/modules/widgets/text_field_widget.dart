60ab3ac880fc0175ebcab269dc6f24fc23b320b1
marcelacrvalho <marcelacrvalho@icloud.com>
2022-11-01 14:14:05 -0300

    Torna onDone nullable

--- lib/app/modules/widgets/text_field_widget.dart
+++ lib/app/modules/widgets/text_field_widget.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class CustomTextFieldWidget extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-  final Function() onDone;
+  final Function()? onDone;
 
   const CustomTextFieldWidget(
       {Key? key, required this.controller, 
       required this.hintText,
-       required this.onDone,})
+      this.onDone,})
       : super(key: key);
 
   @override
@@ -21,7 +19,7 @@ class CustomTextFieldWidget extends StatelessWidget {
       decoration: InputDecoration(
         hintText: hintText,
         border: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey),
+          borderSide: BorderSide(color: Colors.white),
         ),
       ),
     );
