57630838d6fdf66e3129ce693cbe2c2382637b0d
marcelacrvalho <marcelacrvalho@icloud.com>
2022-11-01 11:42:11 -0300

    Adiciona novo atributo onDone

--- lib/app/modules/widgets/text_field_widget.dart
+++ lib/app/modules/widgets/text_field_widget.dart
@@ -5,15 +5,19 @@ import 'package:flutter/src/widgets/framework.dart';
 class CustomTextFieldWidget extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
+  final Function() onDone;
 
   const CustomTextFieldWidget(
-      {Key? key, required this.controller, required this.hintText})
+      {Key? key, required this.controller, 
+      required this.hintText,
+       required this.onDone,})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
+      onEditingComplete: onDone,
       decoration: InputDecoration(
         hintText: hintText,
         border: const OutlineInputBorder(
